ad2d0f443f4544d929aefa7e4d175f87e8a3f430
Raiane Fernandes <raiane_raiodesol@hotmail.com>
2022-03-11 17:12:59 -0300

    Salvar avaliação

--- backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
+++ backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -11,15 +11,21 @@ import com.devsuperior.dsmovie.entities.Movie;
 import com.devsuperior.dsmovie.repositories.MovieRepository;
 
 @Service
-public class MovieService {
-	
+public class MovieService{
 	@Autowired
 	private MovieRepository repository;
-	@Transactional(readOnly = true)
-	public Page<MovieDTO> findall(Pageable pageable){
-		Page<Movie> result= repository.findAll(pageable);	
-		Page<MovieDTO> page = result.map(x -> new MovieDTO(x));
+	
+	@Transactional (readOnly=true)
+	public Page<MovieDTO> findAll(Pageable pageable) {
+		Page<Movie> result = repository.findAll(pageable);
+		Page<MovieDTO> page= result.map(x -> new MovieDTO(x));
 		return page;
 	}
-
+	
+	@Transactional (readOnly=true)
+	public MovieDTO findById(Long id) {
+		Movie result = repository.findById(id).get();
+		MovieDTO dto = new MovieDTO(result);
+		return dto;
+	}
 }
